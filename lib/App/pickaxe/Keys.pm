17a8f5b3c7f9b0a30d766f20eef9d78ee910d7b2
Mario Domgoergen <mario@domgoergen.com>
2022-08-25 20:43:01 +0200

    Use easier to type keybindings and add some getline functions

---
+++ lib/App/pickaxe/Keys.pm
@@ -0,0 +1,42 @@
+package App::pickaxe::Keys;
+use Mojo::Base 'Exporter', -signatures;
+use Curses;
+
+our @EXPORT_OK = ('getkey');
+
+my %table = (
+    Curses::KEY_BACKSPACE => '<Backspace>',
+    Curses::KEY_LEFT      => '<Left>',
+    Curses::KEY_RIGHT     => '<Right>',
+    Curses::KEY_UP        => '<Up>',
+    Curses::KEY_DOWN      => '<Down>',
+    "\n"                  => '<Return>',
+);
+
+sub getkey {
+        my $key = getchar;
+        if ( exists $table{$key} ) {
+            $key = $table{$key};
+        }
+        elsif ( $key eq "" ) {
+            nodelay( stdscr, 1 );
+            my $mod = getchar;
+            if ( $mod ) {
+                $key = '<Esc>' . translate($mod);
+            }
+            else {
+                $key = '<Esc>';
+            }
+            nodelay( stdscr, 0 );
+        }
+        else {
+            $key = unctrl($key);
+        }
+        return $key;
+}
+
+sub translate ($key) {
+    $table{$key} || $key;
+}
+
+1;
