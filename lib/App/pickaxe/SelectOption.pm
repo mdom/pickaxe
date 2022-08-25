da784ca64212e6a26cf7114982fe952561686d49
Mario Domgoergen <mario@domgoergen.com>
2022-08-25 10:54:47 +0200

    Autogenerate select_option

--- lib/App/pickaxe/SelectOption.pm
+++ lib/App/pickaxe/SelectOption.pm
@@ -2,22 +2,23 @@ package App::pickaxe::SelectOption;
 use Mojo::Base -signatures, 'Exporter';
 use Curses;
 
-our @EXPORT_OK = ('select_option', 'askyesno');
+our @EXPORT_OK = ( 'select_option', 'askyesno' );
 
-sub askyesno ( $question ) {
-    select_option("$question ([yes]/no): ", { y => 1, n => 0, "\n" => 1 });
+sub askyesno ($question) {
+    select_option( $question, qw(Yes No)) eq 'yes' ? 1 : 0;
 }
 
-sub select_option ( $prompt, $options ) {
+sub select_option ( $prompt, @options ) {
     move( $LINES - 1, 0 );
     clrtoeol;
-    addstring($prompt);
+    addstring("$prompt " . join('/', @options) . '?: ');
+    my %options = map { /([A-Z])/; lc($1) => lc($_) } @options;
     while (1) {
         my $key = getchar;
-        if ( exists $options->{$key} ) {
+        if ( exists $options{$key} ) {
             move( $LINES - 1, 0 );
             clrtoeol;
-            return $options->{$key};
+            return $options{$key};
         }
     }
 }
