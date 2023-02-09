4d6ebe70101899c345312b6aa85c1eae954c6274
Mario Domgoergen <mdom@taz.de>
2023-02-09 09:26:26 +0100

    Delete unused package App::pickaxe::Pages

--- lib/App/pickaxe/Index.pm
+++ lib/App/pickaxe/Index.pm
@@ -14,7 +14,6 @@ use Mojo::Util 'decode', 'encode';
 use POSIX 'strftime';
 
 has 'config';
-has 'keybindings';
 has pages => sub { [] };
 
 has helpbar =>
@@ -30,7 +29,7 @@ sub statusbar ($self) {
     return "pickaxe: $base";
 }
 
-sub current_page ( $self ) {
+sub current_page ($self) {
     $self->pages->[ $self->current_line ];
 }
 
@@ -46,6 +45,7 @@ sub format_time ( $self, $time ) {
     strftime( $strftime_fmt, $sec, $min, $hour, $mday, $mon, $year );
 }
 
+<<<<<<< HEAD
 sub open_in_browser ( $self, $key ) {
     my $page = $self->current_page;
     return if !$page;
@@ -250,10 +250,201 @@ sub query_connection_details ($self) {
     }
 }
 
-sub add_attachment ( $self, $key ) {
-    my $file = getline('Attach file: ');
+sub open_in_browser ( $self, $key ) {
+    return if $self->empty;
+    use IPC::Cmd;
+    IPC::Cmd::run( command => [ 'xdg-open', $self->current_page->url ] );
+}
+
+sub yank_url ( $self, @ ) {
+    my $url = $self->pages->current->url;
+    open( my $xclip, '|-', @{ $self->config->yank_cmd } )
+      or $self->display_msg("Can't yank url: $!");
+    print $xclip $url;
+    close $xclip;
+    $self->display_msg("Copied url to clipboard.");
+}
+
+sub add_page ( $self, $key ) {
+    state $history = [];
+    my $title = getline( "Page name: ", { history => $history } );
+    if ( !$title ) {
+        $self->display_msg("Aborted.");
+        return;
+    }
+    if ( $self->api->page($title) ) {
+        $self->display_msg("Title has already been taken.");
+        return;
+    }
+    my $new_text = $self->call_editor(tempfile);
+
+    if ($new_text) {
+        if ( askyesno("Save page $title?") ) {
+            $self->api->save( $title, $new_text );
+            $self->update_pages;
+            $self->display_msg('Saved.');
+        }
+        else {
+            $self->display_msg('Not saved.');
+        }
+    }
+    else {
+        $self->display_msg('Discard unmodified page.');
+    }
+}
+
+sub save_page ( $self, $title, $new_text, $version = undef ) {
+    while (1) {
+        if ( $self->api->save( $title, $new_text, $version ) ) {
+            $self->display_msg('Saved.');
+        }
+        else {
+            my $option =
+              select_option( 'Conflict detected', qw(Edit Abort Overwrite) );
+            if ( !defined $option or $option eq 'abort' ) {
+                $self->display_msg('Not saved.');
+            }
+            elsif ( $option eq 'edit' ) {
+                ( $new_text, $version ) =
+                  $self->handle_conflict( $title, $new_text );
+                if ( defined $new_text ) {
+                    next;
+                }
+                $self->display_msg('Not saved.');
+            }
+            elsif ( $option eq 'overwrite' ) {
+                $self->api->save( $title, $new_text );
+                $self->display_msg('Saved.');
+            }
+        }
+        last;
+    }
+}
+
+sub edit_page ( $self, $key ) {
+    return if $self->empty;
+
+    my $page = $self->api->page( $self->current_page->{title} );
+
+    my $title   = $page->{title};
+    my $version = $page->{version};
+    my $text    = $page->{text};
+
+    $text =~ s/\r//g;
+    my $tempfile = tempfile;
+    $tempfile->spurt( encode( 'utf8', $text ) );
+
+    my $new_text = $self->call_editor($tempfile);
+    if ( $text ne $new_text ) {
+        if ( askyesno("Save page $title?") ) {
+            $self->save_page( $title, $new_text, $version );
+        }
+        else {
+            $self->display_msg('Not saved.');
+        }
+    }
+    else {
+        $self->display_msg('Discard unmodified page.');
+    }
+    $self->pages->[ $self->current_line ] = $self->api->page( $title );
+}
+
+sub handle_conflict ( $self, $title, $old_text ) {
+    my $page     = $self->api->page($title);
+    my $new_text = $page->{text};
+    $new_text =~ s/\r//g;
+    my $version = $page->{version};
+
+    my @seq1 = split( /\n/, $old_text );
+    my @seq2 = split( /\n/, $new_text );
+
+    my $diff = Algorithm::Diff->new( \@seq1, \@seq2 );
+
+    my $diff_output = '';
+    while ( $diff->Next ) {
+        if ( my @context = $diff->Same ) {
+            $diff_output .= " $_\n" for @context;
+            next;
+        }
+        $diff_output .= "-$_\n" for $diff->Items(1);
+        $diff_output .= "+$_\n" for $diff->Items(2);
+    }
+
+    my $tempfile = tempfile;
+    $tempfile->spurt( encode( 'utf8', $diff_output ) );
+    my $resolved_text;
+    while (1) {
+        $resolved_text = $self->call_editor($tempfile);
+        if ( $resolved_text =~ /^(?:\+|\-)/sm ) {
+            my $option =
+              select_option( 'Unresolved conflicts', qw(Edit Abort) );
+            if ( !defined $option or $option eq 'abort' ) {
+                return;
+            }
+            elsif ( $option eq 'edit' ) {
+                next;
+            }
+        }
+        last;
+    }
+    $resolved_text =~ s/^ //smg;
+    return $resolved_text, $version;
+}
+
+sub call_editor ( $self, $file ) {
     endwin;
-    die $file;
+    my $editor = $ENV{VISUAL} || $ENV{EDITOR} || 'vi';
+    system( $editor, $file->to_string );
+    # $self->render;
+    return decode( 'utf8', $file->slurp );
+}
+
+sub display_help ( $self, $key ) {
+    endwin;
+    system( 'perldoc', $0 );
+    refresh;
+}
+
+sub delete_page ( $self, $key ) {
+    return if $self->empty;
+    my $title = $self->current_page->{title};
+    if ( askyesno("Delete page $title?") ) {
+        if ( my $error = $self->api->delete($title) ) {
+            $self->display_msg("Error: $error");
+            return;
+        }
+        delete $self->pages->[ $self->current_line ];
+        $self->update_pages;
+        $self->display_msg("Deleted.");
+    }
+}
+
+sub query_connection_details ($self) {
+    my $apikey = $self->config->{apikey} || $ENV{REDMINE_APIKEY};
+    if ($apikey) {
+        $self->api->base_url->query( key => $apikey );
+    }
+    else {
+        my $username = $self->config->{username} || getline("Username: ");
+        if ( !$username ) {
+            die "No username was provided.";
+        }
+        my $password;
+        if ( @{ $self->config->pass_cmd } ) {
+            endwin;
+            my $cmd = "@{$self->config->{pass_cmd}}";
+            $password = qx($cmd);
+            chomp($password);
+        }
+        else {
+            $password = $self->config->{password}
+              || getline( "Password: ", { password => 1 } );
+        }
+        if ( !$password ) {
+            die "No password was provided.";
+        }
+        $self->api->base_url->userinfo("$username:$password");
+    }
 }
 
 sub compile_index_format ($self) {
@@ -334,7 +525,7 @@ sub set_pages ( $self, $pages ) {
     my ( $fmt, @args ) = $self->compile_index_format;
     my @lines;
     my $x = 1;
-    for my $page ($self->pages->@*) {
+    for my $page ( $self->pages->@* ) {
         $page->{index} = $x++;
         push @lines, sprintf( $fmt, map { $_->($page) } @args );
     }
@@ -393,7 +584,19 @@ sub switch_project ( $self, $key ) {
 }
 
 sub update_pages ($self) {
+    $self->update_pages;
+}
+
+sub update_pages ($self) {
+    my $line = $self->current_line;
     $self->set_pages( $self->api->pages );
+    $line = $line > $self->nlines - 1 ? $self->nlines - 1 : $line;
+    $self->current_line($line);
+}
+
+sub sync_pages ( $self, $key ) {
+    $self->update_pages;
+    $self->display_msg("Updated.");
 }
 
 sub sync_pages ( $self, $key ) {
