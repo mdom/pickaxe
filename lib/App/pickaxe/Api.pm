9532c3cb5328368d9f1c5165a21ee3abbfe7ca88
Mario Domgoergen <mario@domgoergen.com>
2023-02-10 10:50:20 +0100

    Move render_text to page class

--- lib/App/pickaxe/Api.pm
+++ lib/App/pickaxe/Api.pm
@@ -154,6 +154,8 @@ sub search ( $self, $query ) {
 
 package App::pickaxe::Api::Page;
 use Mojo::Base -base, -signatures;
+use Text::Wrap 'wrap';
+use Mojo::Util 'html_unescape', 'tablify';
 
 has 'title';
 has 'version';
@@ -178,10 +180,69 @@ has 'text' => sub ($self) {
     return '';
 };
 
-sub url ($self) {
+has url => sub ($self) {
     my $url = $self->api->base_url->clone->path( "wiki/" . $self->title );
     $url->query( key => undef );
     return $url->to_string;
-}
+};
+
+has rendered_text => sub ( $self ) {
+
+    my $text = $self->text;
+
+    ## Move <pre> to it's own line
+    $text =~ s/^(\S+)(<\/?pre>)/$1\n$2/gms;
+    $text =~ s/(<\/?pre>)(\S+)$/$1\n$2/gms;
+
+    ## Remove empty lists
+    $text =~ s/^\s*[\*\#]\s*\n//gmsx;
+
+    ## Unscape html entities
+    $text = html_unescape($text);
+
+    # Remove header ids
+    $text =~ s/^h(\d)\(.*?\)\./h$1./gms;
+
+    ## Collapse empty lines;
+    $text =~ s/\n{3,}/\n\n\n/gs;
+
+    my @table;
+    my $pre_mode = 0;
+    my @lines;
+    for my $line ( split( "\n", $text ) ) {
+        if ( $line =~ /<pre>/ ) {
+            $pre_mode = 1;
+        }
+        elsif ( $line =~ /<\/pre>/ ) {
+            $pre_mode = 0;
+        }
+        elsif ($pre_mode) {
+            push @lines, "    " . $line;
+        }
+        elsif ( $line =~ /^\s*\|(.*)\|\s*$/ ) {
+            push @table,
+              [ map { s/_\.//; s/^\s*//; s/\s*$//; $_ } split( '\|', $1 ) ];
+        }
+        elsif (@table) {
+            push @lines, split( "\n", tablify( \@table ) );
+            undef @table;
+            redo;
+        }
+        elsif ( $line =~ /^(\s*[\*\#]\s*)\S/ ) {
+            push @lines, split( "\n", wrap( '', ' ' x length($1), $line ) );
+        }
+        elsif ( $line eq '' ) {
+            push @lines, $line;
+        }
+        else {
+            $line =~ /^(\s*)/;
+            push @lines, split( "\n", wrap( $1, $1, $line ) );
+        }
+    }
+    if (@table) {
+        push @lines, split( "\n", tablify( \@table ) );
+    }
+    return join("\n",@lines);
+};
 
 1;
--- lib/App/pickaxe/Pager.pm
+++ lib/App/pickaxe/Pager.pm
@@ -1,10 +1,5 @@
 package App::pickaxe::Pager;
 use Mojo::Base -signatures, 'App::pickaxe::GUI::Pager';
-use Curses;
-use App::pickaxe::Getline 'getline';
-use Mojo::Util 'decode', 'encode';
-use Text::Wrap 'wrap';
-use Mojo::Util 'html_unescape', 'tablify';
 
 my @delegate =
   qw(api open_in_browser add_page edit_page display_help delete_page add_attachment update_pages set_order set_reverse_order view_page search switch_project);
@@ -42,70 +37,13 @@ sub prev_item ( $self, $key ) {
     $self->index->prev_item($key);
 }
 
-sub render_text ( $self, $text ) {
-
-    ## Move <pre> to it's own line
-    $text =~ s/^(\S+)(<\/?pre>)/$1\n$2/gms;
-    $text =~ s/(<\/?pre>)(\S+)$/$1\n$2/gms;
-
-    ## Remove empty lists
-    $text =~ s/^\s*[\*\#]\s*\n//gmsx;
-
-    ## Unscape html entities
-    $text = html_unescape($text);
-
-    # Remove header ids
-    $text =~ s/^h(\d)\(.*?\)\./h$1./gms;
-
-    ## Collapse empty lines;
-    $text =~ s/\n{3,}/\n\n\n/gs;
-
-    my @table;
-    my $pre_mode = 0;
-    my @lines;
-    for my $line ( split( "\n", $text ) ) {
-        if ( $line =~ /<pre>/ ) {
-            $pre_mode = 1;
-        }
-        elsif ( $line =~ /<\/pre>/ ) {
-            $pre_mode = 0;
-        }
-        elsif ($pre_mode) {
-            push @lines, "    " . $line;
-        }
-        elsif ( $line =~ /^\s*\|(.*)\|\s*$/ ) {
-            push @table,
-              [ map { s/_\.//; s/^\s*//; s/\s*$//; $_ } split( '\|', $1 ) ];
-        }
-        elsif (@table) {
-            push @lines, split( "\n", tablify( \@table ) );
-            undef @table;
-            redo;
-        }
-        elsif ( $line =~ /^(\s*[\*\#]\s*)\S/ ) {
-            push @lines, split( "\n", wrap( '', ' ' x length($1), $line ) );
-        }
-        elsif ( $line eq '' ) {
-            push @lines, $line;
-        }
-        else {
-            $line =~ /^(\s*)/;
-            push @lines, split( "\n", wrap( $1, $1, $line ) );
-        }
-    }
-    if (@table) {
-        push @lines, split( "\n", tablify( \@table ) );
-    }
-    return @lines;
-}
-
 has 'old_page';
 
 sub render ($self) {
     if ( $self->old_page ne $self->index->current_page ) {
         my $page = $self->index->current_page;
         $self->old_page($page);
-        $self->set_lines( $self->render_text( $page->text ) );
+        $self->set_lines( $page->rendered_text );
     }
     $self->next::method;
 }
