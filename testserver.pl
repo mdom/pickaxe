8108644d81da9d5f6690dc8893961c1cbba89f51
Mario Domgoergen <mario@domgoergen.com>
2023-02-17 21:30:46 +0100

    Add support for versions

--- testserver.pl
+++ testserver.pl
@@ -16,27 +16,28 @@ for my $file (@files) {
     my $mtime    = Mojo::Date->new( $file->stat->mtime )->to_datetime;
     my ( $uid, $name ) = ( getpwuid( $file->stat->uid ) )[ 2, 6 ];
 
-    $pages{$basename} = {
+    push @{ $pages{$basename} },
+      {
         title      => $basename,
         text       => decode( 'utf-8', $file->slurp ),
         author     => { id => $uid, name => $name },
         comments   => '',
         created_on => $mtime,
         updated_on => $mtime,
-        version    => 0,
-    };
+        version    => 1,
+      };
 }
 
 get '/projects/foo/wiki/index', [ format => ['json'] ] => sub {
     my $c = shift;
-    $c->render( json => { wiki_pages => [ values %pages ] } );
+    $c->render( json => { wiki_pages => [ map { $_->[-1] } values %pages ] } );
 };
 
 get '/projects/foo/wiki/:version/:title', [ format => ['json'] ] => sub {
     my $c    = shift;
     my $page = $pages{ $c->stash('title') };
     if ($page) {
-        $c->render( json => { wiki_page => $page } );
+        $c->render( json => { wiki_page => $page->[ $c->stash('version') ] } );
     }
     else {
         $c->render( status => 404, text => '' );
@@ -47,7 +48,7 @@ get '/projects/foo/wiki/:title', [ format => ['json'] ] => sub {
     my $c    = shift;
     my $page = $pages{ $c->stash('title') };
     if ($page) {
-        $c->render( json => { wiki_page => $page } );
+        $c->render( json => { wiki_page => $page->[-1] } );
     }
     else {
         $c->render( status => 404, text => '' );
@@ -58,26 +59,41 @@ put '/projects/foo/wiki/:title', [ format => ['json'] ] => sub {
     my $c     = shift;
     my $title = $c->stash('title');
     my $text  = $c->req->json->{wiki_page}->{text};
+    my $comment  = $c->req->json->{wiki_page}->{comment} || '';
+    my ( $uid, $name ) = ( getpwuid($<) )[ 2, 6 ];
+    my $author = { id => $uid, name => $name };
 
     my $time = Mojo::Date->new->to_datetime;
     if ( my $page = $pages{$title} ) {
-        $page->{text} = $text, $page->{updated_on} = $time;
-        use Data::Dumper;
-        warn Dumper $page;
+
+        my $version = $c->req->json->{wiki_page}->{version};
+        if ( defined $version && $version < @{ $pages{$title} } ) {
+            $c->render( status => 409, text => '' );
+            return;
+        }
+
+        my $new_page = { %{ $page->[-1] } };
+        $new_page->{text}       = $text;
+        $new_page->{updated_on} = $time;
+        $new_page->{author}     = $author;
+        $new_page->{comment}    = $comment;
+        $new_page->{version}++;
+
+        push @{ $pages{$title} }, $new_page;
 
         $c->render( status => 204, text => '' );
     }
     else {
-        my ( $uid, $name ) = ( getpwuid($<) )[ 2, 6 ];
-        $pages{$title} = {
+        push @{ $pages{$title} },
+          {
             title      => $title,
             text       => $text,
-            author     => { id => $uid, name => $name },
-            comments   => '',
+            author     => $author,
+            comments   => $comment,
             created_on => $time,
             updated_on => $time,
-            version    => 0,
-        };
+            version    => 1,
+          };
         $c->render( status => 201, text => '' );
     }
 };
@@ -97,7 +113,7 @@ get '/projects/foo/search', [ format => ['json'] ] => sub {
     my @results = map {
         $_->{description} = delete $_->{text};
         $_
-    } grep { $_->{text} =~ /\Q$q/ } values %pages;
+    } grep { $_->{text} =~ /\Q$q/ } map { $_->[-1] } values %pages;
 
     $c->render(
         json => {
