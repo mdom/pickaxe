665474adf3d5a8adeeff9f4080a473fb1e4405cc
Mario Domgoergen <mario@domgoergen.com>
2023-02-21 14:12:36 +0100

    wip

--- lib/App/pickaxe/Api.pm
+++ lib/App/pickaxe/Api.pm
@@ -1,6 +1,8 @@
 package App::pickaxe::Api;
 use Mojo::Base -signatures, -base;
+
 use Mojo::UserAgent;
+use App::pickaxe::Page;
 
 has base_url => sub {
     die "App::pickaxe::Api->base_url undefined.";
@@ -161,120 +163,5 @@ sub search ( $self, $query ) {
     }
     return \@matching_pages;
 }
-
-package App::pickaxe::Page;
-use Mojo::Base -base, -signatures;
-use Text::Wrap 'wrap';
-use Mojo::Util 'html_unescape', 'tablify';
-
-has 'title';
-has 'version';
-has 'created_on';
-has 'updated_on';
-
-has 'api', undef, weak => 1;
-
-sub fetch_all ( $self ) {
-    my $version = $self->version;
-    my $title   = $self->title;
-    my $res     = $self->api->get("wiki/$title/$version.json");
-    if ( $res->is_success ) {
-        my $page = $res->json->{wiki_page};
-        $page->{text} =~ s/\r\n/\n/gs;
-        for my $key ( keys %$page ) {
-            $self->$key( $page->{$key} );
-        }
-    }
-    return;
-}
-
-has parent => sub ($self) {
-    $self->fetch_all;
-    return $self->parent;
-};
-
-has comments => sub ($self) {
-    $self->fetch_all;
-    return $self->comments;
-};
-
-has author => sub ($self) {
-    $self->fetch_all;
-    return $self->author;
-};
-
-has text => sub ($self) {
-    $self->fetch_all;
-    return $self->text;
-};
-
-has url => sub ($self) {
-    my $url = $self->api->base_url->clone->path( "wiki/" . $self->title );
-    $url->query( key => undef );
-    return $url->to_string;
-};
-
-has rendered_text => sub ( $self ) {
-
-    my $text = $self->text;
-
-    ## Move <pre> to it's own line
-    $text =~ s/^(\S+)(<\/?pre>)/$1\n$2/gms;
-    $text =~ s/(<\/?pre>)(\S+)$/$1\n$2/gms;
-
-    ## Remove empty lists
-    $text =~ s/^\s*[\*\#]\s*\n//gmsx;
-
-    ## Unscape html entities
-    $text = html_unescape($text);
-
-    # Remove header ids
-    $text =~ s/^h(\d)\(.*?\)\./h$1./gms;
-
-    ## Collapse empty lines;
-    $text =~ s/\n{3,}/\n\n\n/gs;
-    $text =~ s/\r\n/\n/g;
-
-    my @table;
-    my $pre_mode = 0;
-    my @lines;
-    use Data::Dumper;
-    # if ($self->version == 235 ) { die Dumper [ split("\n",  $text ) ] };
-    for my $line ( split( "\n", $text ) ) {
-        if ( $line =~ /<pre>/ ) {
-            $pre_mode = 1;
-        }
-        elsif ( $line =~ /<\/pre>/ ) {
-            $pre_mode = 0;
-        }
-        elsif ($pre_mode) {
-            push @lines, "    " . $line;
-        }
-        elsif ( $line =~ /^\s*\|(.*)\|\s*$/ ) {
-            push @table,
-              [ map { s/_\.//; s/^\s*//; s/\s*$//; $_ } split( '\|', $1 ) ];
-        }
-        elsif (@table) {
-            push @lines, split( "\n", tablify( \@table ) );
-            undef @table;
-            redo;
-        }
-        elsif ( $line =~ /^(\s*[\*\#]\s*)\S/ ) {
-            push @lines, split( "\n", wrap( '', ' ' x length($1), $line ) );
-        }
-        elsif ( $line eq '' ) {
-            push @lines, $line;
-        }
-        else {
-            $line =~ /^(\s*)/;
-            push @lines, split( "\n", wrap( $1, $1, $line ) );
-        }
-    }
-    if (@table) {
-        push @lines, split( "\n", tablify( \@table ) );
-    }
-    return join("\n",@lines);
-
-};
 
 1;
